fcc4cd30101876d49796902bc9a7a97d8182eb9a
DANGEERJI <goswamireyansh9896@gmail.com>
2025-08-25 10:20:03 +0530

    Updated Code

--- Experiment 2/exp2.cpp
+++ Experiment 2/exp2.cpp
@@ -5,20 +5,29 @@
 
 using namespace std;
 
-int power(int base, int exp) {
-   if (exp == 0)
-      return 1;
-   int half = power(base, exp / 2);
-   if (exp % 2 == 0)
-      return half * half;
-   else
-      return base * half * half;
+double myPow(double x, int n) {
+   long long exp = n;
+   if (exp < 0) {
+      x = 1 / x;
+      exp = -exp;
+   }
+
+   double result = 1.0;
+   while (exp > 0) {
+      if (exp % 2 == 1) {
+            result *= x;
+      }
+      x *= x;
+      exp /= 2;
+   }
+
+   return result;
 }
 
 int main() {
    int base = 3;
    int exp = 9;
-   int result = power(base, exp);
+   int result = myPow(base, exp);
    cout << "Base: " << base << endl;
    cout << "Exponent: " << exp << endl;
    cout << "Result: " << result << endl;
